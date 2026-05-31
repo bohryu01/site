9d2f19834c04a41d999faaa0e8e962367ed812a1
bohryu01 <276181267@qq.com>
2026-05-31 17:08:14 +0800

    Update index.js
    update index.js to return HTML page

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 export default {
   async fetch(request) {
-    return new Response("Hello from Cloudflare Worker!", {
-      headers: { "Content-Type": "text/plain" }
+    const html = `
+      <!DOCTYPE html>
+      <html>
+        <head>
+          <meta charset="UTF-8">
+          <title>My Cloudflare Worker Site</title>
+        </head>
+        <body>
+          <h1>Hello from Cloudflare Worker!</h1>
+          <p>Your site is live at site.yubo169.workers.dev</p>
+        </body>
+      </html>
+    `;
+    return new Response(html, {
+      headers: { "Content-Type": "text/html" }
     });
   }
 };
